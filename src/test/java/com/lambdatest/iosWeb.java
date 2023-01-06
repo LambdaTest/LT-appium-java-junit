127cf37369bc54e3517a2f7eaa6744ddf236a61b
Ankit Agarwal <ankitagarwal@lambdatest.com>
2023-01-07 01:00:11 +0530

    adding web automation

---
+++ src/test/java/com/lambdatest/iosWeb.java
@@ -0,0 +1,83 @@
+package com.lambdatest;
+
+import io.appium.java_client.MobileBy;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class iosWeb {
+
+    String username = System.getenv("LT_USERNAME") == null ? "LT_USERNAME"   //Enter the Username here
+            : System.getenv("LT_USERNAME");
+    String accessKey = System.getenv("LT_ACCESS_KEY") == null ? "LT_ACCESS_KEY"   //Enter the Access key here
+            : System.getenv("LT_ACCESS_KEY");
+    public static RemoteWebDriver driver = null;
+    public String gridURL = "@mobile-hub.lambdatest.com/wd/hub";
+    public String status = "passed";
+    @Before
+    public void setUp() throws Exception {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("build", "JUNIT Native Web automation");
+        capabilities.setCapability("name", "Java JUnit iOS iPhone 12");
+        capabilities.setCapability("platformName", "ios");
+        capabilities.setCapability("deviceName", "iPhone .*");
+        capabilities.setCapability("isRealMobile", true);
+        capabilities.setCapability("platformVersion","15");
+        capabilities.setCapability("deviceOrientation", "PORTRAIT");
+        capabilities.setCapability("console",true);
+        capabilities.setCapability("network",false);
+        capabilities.setCapability("visual",true);
+        try
+        {
+            driver = new RemoteWebDriver(new URL("https://" + username + ":" + accessKey + gridURL), capabilities);
+        }
+        catch (MalformedURLException e)
+        {
+            System.out.println("Invalid grid URL");
+        } catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testSimple() throws Exception
+    {
+        try
+        {
+            driver.get("https://mfml.in/api/getInfo");
+            WebDriverWait wait = new WebDriverWait(driver, 30);
+            wait.until(ExpectedConditions.elementToBeClickable(By.id("resolution"))).click();
+
+            wait.until(ExpectedConditions.elementToBeClickable(By.id("location"))).click();
+            wait.until(ExpectedConditions.elementToBeClickable(By.id("details"))).click();
+            wait.until(ExpectedConditions.elementToBeClickable(By.id("timezone"))).click();
+
+            status="passed";
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            status="failed";
+        }
+    }
+    @After
+    public void tearDown() throws Exception
+    {
+        if (driver != null)
+        {
+            driver.executeScript("lambda-status=" + status);
+            driver.quit();
+        }
+    }
+
+}

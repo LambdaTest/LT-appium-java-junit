1f0a65c70db9835af6612cac0cf10b09d0242cc2
Berk TURKUCU <95139164+turkucuberk@users.noreply.github.com>
2023-02-13 16:13:25 +0300

    add android w3c

---
+++ src/test/java/com/lambdatest/android_w3c.java
@@ -0,0 +1,99 @@
+package com.lambdatest;
+
+import io.appium.java_client.MobileBy;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class android_w3c {
+    String username = System.getenv("LT_USERNAME") == null ? "LT_USERNAME" //Enter the Username here
+            : System.getenv("LT_USERNAME");
+    String accessKey = System.getenv("LT_ACCESS_KEY") == null ? "LT_ACCESS_KEY"  //Enter the Access key here
+            : System.getenv("LT_ACCESS_KEY");
+    public static RemoteWebDriver driver = null;
+    public String gridURL = "@mobile-hub.lambdatest.com/wd/hub";
+    public String status = "passed";
+    @Before
+    public void setUp() throws Exception {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("build", "JUNIT Native App automation");
+        capabilities.setCapability("name", "Java JUnit Android Pixel 6_w3c");
+        capabilities.setCapability("platformName", "android");
+        capabilities.setCapability("deviceName", "Pixel 6"); //Enter the name of the device here
+        capabilities.setCapability("isRealMobile", true);
+        capabilities.setCapability("platformVersion","12");
+        capabilities.setCapability("app","lt://proverbial-android"); //Enter the App ID here
+        capabilities.setCapability("deviceOrientation", "PORTRAIT");
+        capabilities.setCapability("console",true);
+        capabilities.setCapability("network",false);
+        capabilities.setCapability("visual",true);
+        capabilities.setCapability("enableCustomTranslation",true);
+        capabilities.setCapability("w3c",true);
+        try
+        {
+            driver = new RemoteWebDriver(new URL("https://" + username + ":" + accessKey + gridURL), capabilities);
+        }
+        catch (MalformedURLException e)
+        {
+            System.out.println("Invalid grid URL");
+        } catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testSimple() throws Exception
+    {
+        try
+        {
+            WebDriverWait wait = new WebDriverWait(driver, 30);
+            wait.until(ExpectedConditions.elementToBeClickable(MobileBy.id("color"))).click();
+
+            wait.until(ExpectedConditions.elementToBeClickable(MobileBy.id("geoLocation"))).click();;
+            Thread.sleep(5000);
+            driver.navigate().back();
+
+            wait.until(ExpectedConditions.elementToBeClickable(MobileBy.id("Text"))).click();
+
+            wait.until(ExpectedConditions.elementToBeClickable(MobileBy.id("notification"))).click();;
+
+            wait.until(ExpectedConditions.elementToBeClickable(MobileBy.id("toast"))).click();
+
+            wait.until(ExpectedConditions.elementToBeClickable(By.id("com.lambdatest.proverbial:id/webview"))).click();;
+            Thread.sleep(10000);
+
+            wait.until(ExpectedConditions.elementToBeClickable(MobileBy.id("url"))).sendKeys("https://www.lambdatest.com/");
+
+            wait.until(ExpectedConditions.elementToBeClickable(MobileBy.id("find"))).click();
+            Thread.sleep(5000);
+            driver.navigate().back();
+
+            status="passed"; 
+        }
+            catch (Exception e)
+             {
+                System.out.println(e.getMessage());
+                status="failed";
+             }
+    }
+    @After
+    public void tearDown() throws Exception
+    {
+        if (driver != null)
+        {
+            driver.executeScript("lambda-status=" + status);
+            driver.quit();
+        }
+    }
+}
+
